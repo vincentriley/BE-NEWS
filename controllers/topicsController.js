df1bdbc24891e89a5a8ca4de19b2de1f8ba45b99
Vincent Riley-Smith <v.riley-smith@outlook.com>
2022-07-04 12:02:45 +0100

    added GET topics endpoint

--- controllers/topicsController.js
+++ controllers/topicsController.js
@@ -2,7 +2,6 @@ const fetchTopics = require("../models/topicsModel")
 
 const getTopics = (req, res, next) => {
     fetchTopics().then((data) => {
-        console.log(data)
         res.status(200).send({topics: data})
     }).catch((err) => {
         next(err)
