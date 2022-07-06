aec26bc96af8156ec7980aeda224664771319e74
Vincent Riley-Smith <v.riley-smith@outlook.com>
2022-07-06 15:16:08 +0100

    commit2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -43,112 +43,112 @@ describe("GET /api/topics", () => {
 	});
 });
 
-// describe("GET /api/articles/:article_id", () => {
-// 	test("status:200, responds with article object", () => {
-// 		return request(app)
-// 			.get("/api/articles/2")
-// 			.expect(200)
-// 			.then(({ body }) => {
-// 				const { article } = body;
-// 				expect(article).toEqual({
-// 					article_id: 2,
-// 					title: expect.any(String),
-// 					topic: expect.any(String),
-// 					author: expect.any(String),
-// 					body: expect.any(String),
-// 					created_at: expect.any(String),
-// 					votes: expect.any(Number),
-// 					comment_count: expect.any(Number),
-// 				});
-// 			});
-// 	});
-// 	test("status:404, responds with 404 not found when passed invalid id", () => {
-// 		return request(app)
-// 			.get("/api/articles/999999")
-// 			.expect(404)
-// 			.then(({ body }) => {
-// 				const { msg } = body;
-// 				expect(msg).toEqual("Not found");
-// 			});
-// 	});
-// 	test("status:400, responds with 404 not found when passed id that is not number", () => {
-// 		return request(app)
-// 			.get("/api/articles/banana")
-// 			.expect(400)
-// 			.then(({ body }) => {
-// 				const { msg } = body;
-// 				expect(msg).toEqual("Bad request");
-// 			});
-// 	});
-// });
+describe("GET /api/articles/:article_id", () => {
+	test("status:200, responds with article object", () => {
+		return request(app)
+			.get("/api/articles/2")
+			.expect(200)
+			.then(({ body }) => {
+				const { article } = body;
+				expect(article).toEqual({
+					article_id: 2,
+					title: expect.any(String),
+					topic: expect.any(String),
+					author: expect.any(String),
+					body: expect.any(String),
+					created_at: expect.any(String),
+					votes: expect.any(Number),
+					comment_count: expect.any(Number),
+				});
+			});
+	});
+	test("status:404, responds with 404 not found when passed invalid id", () => {
+		return request(app)
+			.get("/api/articles/999999")
+			.expect(404)
+			.then(({ body }) => {
+				const { msg } = body;
+				expect(msg).toEqual("Not found");
+			});
+	});
+	test("status:400, responds with 404 not found when passed id that is not number", () => {
+		return request(app)
+			.get("/api/articles/banana")
+			.expect(400)
+			.then(({ body }) => {
+				const { msg } = body;
+				expect(msg).toEqual("Bad request");
+			});
+	});
+});
 
-// describe("PATCH /api/articles/:article_id", () => {
-// 	test("status:200, responds with article object with correctly incremented votes when passed positive value", () => {
-// 		const votesObject = {
-// 			inc_votes: 10,
-// 		};
-// 		return request(app)
-// 			.patch("/api/articles/2")
-// 			.send(votesObject)
-// 			.expect(200)
-// 			.then(({ body }) => {
-// 				const { article } = body;
-// 				expect(article.votes).toBe(10);
-// 			});
-// 	});
-// 	test("status:200, responds with article object with correctly incremented votes when passed negative value", () => {
-// 		const votesObject = {
-// 			inc_votes: -10,
-// 		};
-// 		return request(app)
-// 			.patch("/api/articles/1")
-// 			.send(votesObject)
-// 			.expect(200)
-// 			.then(({ body }) => {
-// 				const { article } = body;
-// 				expect(article.votes).toBe(90);
-// 			});
-// 	});
-// 	test("status:404, responds with 404 not found when passed invalid id", () => {
-// 		const votesObject = {
-// 			inc_votes: -10,
-// 		};
-// 		return request(app)
-// 			.patch("/api/articles/999999")
-// 			.send(votesObject)
-// 			.expect(404)
-// 			.then(({ body }) => {
-// 				const { msg } = body;
-// 				expect(msg).toEqual("Not found");
-// 			});
-// 	});
-// 	test("status:400, responds with 404 not found when passed id that is not number", () => {
-// 		const votesObject = {
-// 			inc_votes: -10,
-// 		};
-// 		return request(app)
-// 			.patch("/api/articles/banana")
-// 			.send(votesObject)
-// 			.expect(400)
-// 			.then(({ body }) => {
-// 				const { msg } = body;
-// 				expect(msg).toEqual("Bad request");
-// 			});
-// 	});
-// 	test("status:400, responds with 404 not found when passed inc_votes that is not number", () => {
-// 		const votesObject = {
-// 			inc_votes: "cheese",
-// 		};
-// 		return request(app)
-// 			.patch("/api/articles/2")
-// 			.send(votesObject)
-// 			.expect(400)
-// 			.then(({ body }) => {
-// 				const { msg } = body;
-// 				expect(msg).toEqual("Bad request");
-// 			});
-// 	});
-// });
+describe("PATCH /api/articles/:article_id", () => {
+	test("status:200, responds with article object with correctly incremented votes when passed positive value", () => {
+		const votesObject = {
+			inc_votes: 10,
+		};
+		return request(app)
+			.patch("/api/articles/2")
+			.send(votesObject)
+			.expect(200)
+			.then(({ body }) => {
+				const { article } = body;
+				expect(article.votes).toBe(10);
+			});
+	});
+	test("status:200, responds with article object with correctly incremented votes when passed negative value", () => {
+		const votesObject = {
+			inc_votes: -10,
+		};
+		return request(app)
+			.patch("/api/articles/1")
+			.send(votesObject)
+			.expect(200)
+			.then(({ body }) => {
+				const { article } = body;
+				expect(article.votes).toBe(90);
+			});
+	});
+	test("status:404, responds with 404 not found when passed invalid id", () => {
+		const votesObject = {
+			inc_votes: -10,
+		};
+		return request(app)
+			.patch("/api/articles/999999")
+			.send(votesObject)
+			.expect(404)
+			.then(({ body }) => {
+				const { msg } = body;
+				expect(msg).toEqual("Not found");
+			});
+	});
+	test("status:400, responds with 404 not found when passed id that is not number", () => {
+		const votesObject = {
+			inc_votes: -10,
+		};
+		return request(app)
+			.patch("/api/articles/banana")
+			.send(votesObject)
+			.expect(400)
+			.then(({ body }) => {
+				const { msg } = body;
+				expect(msg).toEqual("Bad request");
+			});
+	});
+	test("status:400, responds with 404 not found when passed inc_votes that is not number", () => {
+		const votesObject = {
+			inc_votes: "cheese",
+		};
+		return request(app)
+			.patch("/api/articles/2")
+			.send(votesObject)
+			.expect(400)
+			.then(({ body }) => {
+				const { msg } = body;
+				expect(msg).toEqual("Bad request");
+			});
+	});
+});
 
 // describe(" /api/articles/:article_id/comments", () => {
 // 	test("responds with an array of comment objects", () => {
