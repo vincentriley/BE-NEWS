9cb8d3de11381dae40496dd75f7c48792a52ff38
Vincent Riley-Smith <v.riley-smith@outlook.com>
2022-07-06 16:21:03 +0100

    endpoint and tests done

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -4,6 +4,7 @@ const {
 	updateArticleVotes,
 	addComment,
 	fetchArticleComments,
+	removeComment
 } = require("../models/articlesModel");
 
 const getArticles = (req, res, next) => {
@@ -66,10 +67,20 @@ const getArticleComments = (req, res, next) => {
 		});
 };
 
+const deleteComment = (req, res, next) => {
+	const {comment_id : commentId} = req.params
+	removeComment(commentId).then((data) => {
+		res.status(204).send()
+	}).catch((err) => {
+		next(err)
+	})
+}
+
 module.exports = {
 	getArticles,
 	getArticleById,
 	patchArticleById,
 	postComment,
 	getArticleComments,
+	deleteComment
 };
--- models/articlesModel.js
+++ models/articlesModel.js
@@ -84,5 +84,16 @@ const fetchArticleComments = (articleId) => {
 		});
 };
 
-module.exports = { fetchArticles, fetchArticleById, updateArticleVotes, fetchArticleComments, addComment };
+const removeComment = (commentId) => {
+	return db.query("DELETE FROM comments WHERE comment_id = $1", [commentId]).then(({rowCount}) => {
+		if (rowCount === 0) {
+			return Promise.reject({
+				status: 404,
+				msg: "Not found",
+			});
+		}
+	})
+}
+
+module.exports = { fetchArticles, fetchArticleById, updateArticleVotes, fetchArticleComments, addComment, removeComment };
 
